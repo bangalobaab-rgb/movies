bc69197838736965032bb62f33f4df3254fe07d5
bangalobaab-rgb <bangalobaab@gmail.com>
2025-08-12 14:13:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '21484'))
-API_HASH = environ.get('API_HASH', 'db6286a041adf07ef4244e')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7807I9ygow9sLqNQuI7hsWPm5FLNfBr9c')
+API_ID = int(environ.get('API_ID', '23937655'))
+API_HASH = environ.get('API_HASH', '6ad9aed088fa03dd8bb9f4f4d076f1f6')
+BOT_TOKEN = environ.get('BOT_TOKEN', '8487282442:AAHkdOUOYvDUiqnj6E9H9CVuCYRvie92Q3c')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5016109398').split()]
